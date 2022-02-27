efc49a8db0ebc703754a2f66c0a5be52a80ea2b5
danursin <danursin@gmail.com>
2022-02-26 22:46:56 -0600

    refactor: ingredient structure

--- graphql/entities/CookIngredient.ts
+++ graphql/entities/CookIngredient.ts
@@ -1,28 +1,50 @@
 import { CookIngredientTable, RequestContext } from "../../types";
-import { GraphQLFieldConfigMap, GraphQLFloat, GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+import { GraphQLFieldConfigMap, GraphQLFloat, GraphQLInt, GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
 
 import Cook from "./Cook";
-import Ingredient from "./Ingredient";
-import Unit from "./Unit";
 
 const CookIngredient: GraphQLObjectType = new GraphQLObjectType({
     name: "CookIngredient",
     fields: (): GraphQLFieldConfigMap<CookIngredientTable, RequestContext> => ({
         amount: { type: GraphQLFloat },
         description: { type: GraphQLString },
-        unit: {
-            type: Unit,
-            resolve: ({ unit_id }, _args, { dataSources: { dataService } }) => {
+        unit_id: {
+            type: GraphQLInt,
+            resolve: async ({ unit_id }, _args, { dataSources: { dataService } }) => {
                 if (!unit_id) {
                     return null;
                 }
-                return dataService.getUnit(unit_id);
+                return (await dataService.getUnit(unit_id)).id;
             }
         },
-        ingredient: {
-            type: new GraphQLNonNull(Ingredient),
-            resolve: ({ ingredient_id }, _args, { dataSources: { dataService } }) => {
-                return dataService.getIngredient(ingredient_id);
+        unit_title: {
+            type: GraphQLString,
+            resolve: async ({ unit_id }, _args, { dataSources: { dataService } }) => {
+                if (!unit_id) {
+                    return null;
+                }
+                return (await dataService.getUnit(unit_id)).title;
+            }
+        },
+        unit_abbreviation: {
+            type: GraphQLString,
+            resolve: async ({ unit_id }, _args, { dataSources: { dataService } }) => {
+                if (!unit_id) {
+                    return null;
+                }
+                return (await dataService.getUnit(unit_id)).abbreviation;
+            }
+        },
+        ingredient_id: {
+            type: GraphQLInt,
+            resolve: async ({ ingredient_id }, _args, { dataSources: { dataService } }) => {
+                return (await dataService.getIngredient(ingredient_id)).id;
+            }
+        },
+        ingredient_title: {
+            type: GraphQLString,
+            resolve: async ({ ingredient_id }, _args, { dataSources: { dataService } }) => {
+                return (await dataService.getIngredient(ingredient_id)).title;
             }
         },
         cook: {
